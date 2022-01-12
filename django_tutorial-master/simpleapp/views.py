7cfcb61495eb97e73faa7389afb6a090e0bbfc39
PHILLyaHI <87790235+PHILLyaHI@users.noreply.github.com>
2022-01-11 18:45:00 -0800

    added new file

---
+++ django_tutorial-master/simpleapp/views.py
@@ -0,0 +1,25 @@
+from django.views.generic import ListView, DetailView
+from .models import Article
+from django.views import View
+from datetime import datetime
+
+
+class ArticleList(ListView):
+    model = Article
+    template_name = 'articles.html'
+    context_object_name = 'articles'
+    queryset = Article.objects.order_by('-datetime')
+
+    # метод get_context_data нужен нам для того, чтобы мы могли передать переменные в шаблон. В возвращаемом словаре context будут храниться все переменные. Ключи этого словаря и есть переменные, к которым мы сможем потом обратиться через шаблон
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context['time_now'] = datetime.utcnow()  # добавим переменную текущей даты time_now
+        context['value1'] = None  # добавим ещё одну пустую переменную, чтобы на её примере посмотреть работу другого фильтра
+        return context
+
+
+class ArticleDetail(DetailView):
+    model = Article
+    template_name = 'article.html'
+    context_object_name = 'article'
+
